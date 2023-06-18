815a0947386d5bef4e401a8cdd44cdb2c5fc33af
Satish Pokarel <relstudiosnx@gmail.com>
2023-06-18 23:02:50 +0300

    updated cart to checkout word

--- admin/modules/agree-to-terms-checkbox/admin-page-agree-to-terms-checkbox.php
+++ admin/modules/agree-to-terms-checkbox/admin-page-agree-to-terms-checkbox.php
@@ -14,7 +14,7 @@ Merchant_Admin_Options::create( array(
 		array(
 			'id'       => 'text',
 			'type'     => 'text',
-			'title'    => esc_html__( 'Checkbox text on cart page', 'merchant' ),
+			'title'    => esc_html__( 'Checkbox text on checkbox page', 'merchant' ),
 			'desc'     => esc_html__( "Please keep the ##terms_link## magic keyword there, it will be replaced with the link you set below.", 'merchant' ),
 			'default'  => 'I have read, understood and agreed with your terms and conditions. <a href="##terms_link##" target="_blank">terms and conditions</a>',
 			'sanitize' => 'wp_kses_post'
@@ -30,9 +30,9 @@ Merchant_Admin_Options::create( array(
 		array(
 			'id'      => 'warning_text',
 			'type'    => 'text',
-			'title'   => esc_html__( 'Checkbox warning text on cart page', 'merchant' ),
-			'default' => esc_html__( 'You must read and accept the terms and conditions to cart.', 'merchant' ),
-			'desc'    => esc_html__( 'The error that will be shown if the user tries to go to cart without accepting your terms and conditions.', 'merchant' ),
+			'title'   => esc_html__( 'Checkbox warning text on checkbox page', 'merchant' ),
+			'default' => esc_html__( 'You must read and accept the terms and conditions to checkbox.', 'merchant' ),
+			'desc'    => esc_html__( 'The error that will be shown if the user tries to go to checkbox without accepting your terms and conditions.', 'merchant' ),
 		),
 
 	),
