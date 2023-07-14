96ed38375ea59836eba5920990b37f401c7fc785
Rodrigo Teixeira <maaprote@gmail.com>
2023-07-14 17:28:34 -0300

    sticky add to cart

--- inc/functions.php
+++ inc/functions.php
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  * Get template part.
  * 
  */
-function merchant_get_template_part( $module_slug, $name = '', $args = array() ) {
+function merchant_get_template_part( $folder_path = '', $name = '', $args = array() ) {
 	if ( ! empty( $args ) && is_array( $args ) ) {
 		extract( $args );
 	}
@@ -22,23 +22,22 @@ function merchant_get_template_part( $module_slug, $name = '', $args = array() )
 	$plugin_dir = ! defined( 'MERCHANT_PRO_DIR' ) ? MERCHANT_DIR : MERCHANT_PRO_DIR;
 	$template	 = '';
 
-	// Look in yourtheme/module-slug/name.php and yourtheme/merchant/module-slug/name.php.
+	// Look in yourtheme/merchant/folder-path/name.php and yourtheme/merchant/folder-path/name.php.
 	if ( $name ) {
-		$template = locate_template( array( "merchant/{$module_slug}/{$name}.php" ) );
+		$template = locate_template( array( "merchant/{$folder_path}/{$name}.php" ) );
 	}
 
 	// Get default.
-	if ( ! $template && $name && file_exists( $plugin_dir . "inc/modules/{$module_slug}/templates/{$name}.php" ) ) {
-		$template = $plugin_dir . "inc/modules/{$module_slug}/templates/{$name}.php";
+	if ( ! $template && $name && file_exists( $plugin_dir . "templates/{$folder_path}/{$name}.php" ) ) {
+		$template = $plugin_dir . "templates/{$folder_path}/{$name}.php";
 	}
 
 	/**
 	 * Hook: 'merchant_get_template_part'
-	 * Allow 3rd party plugin filter template file from their plugin.
 	 * 
 	 * @since 1.0
 	 */
-	$template = apply_filters( 'merchant_get_template_part', $template, $module_slug, $name );
+	$template = apply_filters( 'merchant_get_template_part', $template, $folder_path, $name );
 
 	if ( $template ) {
 		return include( $template );
