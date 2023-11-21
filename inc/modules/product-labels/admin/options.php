865efd62580ced5f5bb3e38cafb720d591540de1
devwael <dev.ahmedwael@gmail.com>
2023-11-21 23:35:14 +0200

    Building updated product labels module fields

--- inc/modules/product-labels/admin/options.php
+++ inc/modules/product-labels/admin/options.php
@@ -12,33 +12,100 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 Merchant_Admin_Options::create( array(
 	'title'  => esc_html__( 'Settings', 'merchant' ),
-	'module' => 'product-labels',
+	'module' => Merchant_Product_Labels::MODULE_ID,
 	'fields' => array(
-
 		array(
-			'id'      => 'label_text',
-			'type'    => 'text',
-			'title'   => esc_html__( 'Label text', 'merchant' ),
-			'desc'    => esc_html__( 'Note: This label will be shown only on sales products.', 'merchant' ),
-			'default' => esc_html__( 'Spring Special', 'merchant' ),
-		),
+			'id'           => 'labels',
+			'type'         => 'flexible_content',
+			'sorting'      => true,
+			'button_label' => esc_html__( 'Add', 'merchant' ),
+			'layouts'      => array(
+				'single-label' => array(
+					'title'  => esc_html__( 'Products Label', 'merchant' ),
+					'fields' => array(
+						array(
+							'id'      => 'label',
+							'title'   => esc_html__( 'Label', 'merchant' ),
+							'type'    => 'text',
+							'default' => esc_html__( 'Spring Special', 'merchant' ),
+						),
+						array(
+							'id'      => 'pages_to_display',
+							'type'    => 'radio',
+							'title'   => esc_html__( 'Pages to display', 'merchant' ),
+							'options' => array(
+								'both'    => esc_html__( 'Both', 'merchant' ),
+								'archive' => esc_html__( 'archive', 'merchant' ),
+								'single'  => esc_html__( 'Single', 'merchant' ),
+							),
+							'default' => 'both',
+						),
+						array(
+							'id'      => 'display_rules',
+							'type'    => 'select',
+							'title'   => esc_html__( 'Display Rules', 'merchant' ),
+							'options' => array(
+								'featured_products' => esc_html__( 'Featured products', 'merchant' ),
+								'products_on_sale'  => esc_html__( 'Products On Sale', 'merchant' ),
+								'new_products'      => esc_html__( 'New Products', 'merchant' ),
+								'out_of_stock'      => esc_html__( 'Out Of Stock', 'merchant' ),
+								'by_category'       => esc_html__( 'By Product Category', 'merchant' ),
+							),
+							'default' => 'featured_products',
+						),
+						array(
+							'id'        => 'new_products_days',
+							'type'      => 'number',
+							'min'       => 0,
+							'step'      => 1,
+							'title'     => esc_html__( 'How old products', 'merchant' ),
+							'desc'      => esc_html__( 'Set number of days the product will be marked as ‘New’ after the creation', 'merchant' ),
+							'default'   => 3,
+							'condition' => array( 'display_rules', '==', 'new_products' ),
+						),
+						array(
+							'id'        => 'percentage_text',
+							'type'      => 'text',
+							'title'     => esc_html__( 'Sale Percentage', 'merchant' ),
+							'default'   => '-{value}%',
+							'desc'      => esc_html__( 'You may use the {value} tag. E.g. <strong>{value}% OFF!</strong>', 'merchant' ),
+							'condition' => array( 'display_rules', '==', 'products_on_sale' ),
+						),
+						array(
+							'id'        => 'product_cats',
+							'type'      => 'select',
+							'title'     => esc_html__( 'Display Rules', 'merchant' ),
+							'options'   => merchant_get_product_categories(),
+							'condition' => array( 'display_rules', '==', 'by_category' ),
+						),
+						array(
+							'id'      => 'background_color',
+							'type'    => 'color',
+							'title'   => esc_html__( 'Background color', 'merchant' ),
+							'default' => '#212121',
+						),
+						array(
+							'id'      => 'text_color',
+							'type'    => 'color',
+							'title'   => esc_html__( 'Text color', 'merchant' ),
+							'default' => '#ffffff',
+						),
+					),
 
-		array(
-			'id'    => 'display_percentage',
-			'type'  => 'checkbox',
-			'label' => esc_html__( 'Display Sale Percentage', 'merchant' ),
-			'default' => 0
+				),
+			),
+			'default'      => array(
+				array(
+					'layout'           => 'single-label',
+					'label'            => Merchant_Admin_Options::get( Merchant_Product_Labels::MODULE_ID, 'label_text', esc_html__( 'Spring Special', 'merchant' ) ),
+					'pages_to_display' => 'both',
+					'display_rules'    => 'products_on_sale',
+					'percentage_text'  => Merchant_Admin_Options::get( Merchant_Product_Labels::MODULE_ID, 'percentage_text', '-{value}%' ),
+					'background_color' => Merchant_Admin_Options::get( Merchant_Product_Labels::MODULE_ID, 'background_color', '#212121' ),
+					'text_color'       => Merchant_Admin_Options::get( Merchant_Product_Labels::MODULE_ID, 'text_color', '#ffffff' ),
+				),
+			),
 		),
-
-		array(
-			'id'        => 'percentage_text',
-			'type'      => 'text',
-			'title'     => esc_html__( 'Sale percentage text', 'merchant' ),
-			'desc'      => esc_html__( 'You may use the {value} tag. E.g. <strong>{value}% OFF!</strong>', 'merchant' ),
-			'default'   => '-{value}%',
-			'condition' => array( 'display_percentage', '==', '1' ),
-		),
-
 		array(
 			'id'      => 'label_position',
 			'type'    => 'select',
@@ -58,7 +125,7 @@ Merchant_Admin_Options::create( array(
 			'max'     => 35,
 			'step'    => 1,
 			'unit'    => 'px',
-			'default' => 0
+			'default' => 0,
 		),
 
 		array(
@@ -71,7 +138,7 @@ Merchant_Admin_Options::create( array(
 				'capitalize' => esc_html__( 'Capitalize', 'merchant' ),
 				'none'       => esc_html__( 'None', 'merchant' ),
 			),
-			'default' => 'uppercase'
+			'default' => 'uppercase',
 		),
 
 		array(
@@ -95,20 +162,5 @@ Merchant_Admin_Options::create( array(
 			'unit'    => 'px',
 			'default' => 14,
 		),
-
-		array(
-			'id'      => 'background_color',
-			'type'    => 'color',
-			'title'   => esc_html__( 'Background color', 'merchant' ),
-			'default' => '#212121',
-		),
-
-		array(
-			'id'      => 'text_color',
-			'type'    => 'color',
-			'title'   => esc_html__( 'Text color', 'merchant' ),
-			'default' => '#ffffff',
-		),
-
 	),
 ) );
