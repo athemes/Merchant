6008ed10ed8828672762c9a598cb25032d459ecc
Rodrigo Teixeira <maaprote@gmail.com>
2023-11-28 10:57:15 -0300

    copies tweaks

--- inc/modules/wait-list/admin/options.php
+++ inc/modules/wait-list/admin/options.php
@@ -26,7 +26,7 @@ Merchant_Admin_Options::create( array(
 			'id'      => 'form_title',
 			'type'    => 'text',
 			'title'   => esc_html__( 'Form title', 'merchant' ),
-			'default' => __( 'New stock on its way! Email when stock available', 'merchant' ),
+			'default' => __( 'New stock is coming! Email me when this item is back in stock', 'merchant' ),
 		),
 
 		array(
@@ -47,7 +47,7 @@ Merchant_Admin_Options::create( array(
 			'id'      => 'form_success_message',
 			'type'    => 'textarea',
 			'title'   => esc_html__( 'Form success message', 'merchant' ),
-			'default' => __( 'You have been successfully added to our wait list. As soon as new stock become available, we will notify you via email.', 'merchant' ),
+			'default' => __( 'You have been successfully added to our stock waitlist. As soon as new stock becomes available, we will notify you via email.', 'merchant' ),
 			'desc'    => esc_html__( 'The message that will show after form submission.', 'merchant' ),
 		),
 	),
@@ -62,7 +62,7 @@ Merchant_Admin_Options::create( array(
 			'id'      => 'email_new_subscriber',
 			'type'    => 'textarea',
 			'title'   => esc_html__( 'Email new subscribers', 'merchant' ),
-			'default' => __( 'Hello subscriber, Thank you for subscribing to {product}. We will email you once product back in stock.', 'merchant' ),
+			'default' => __( 'Hello, thank you for subscribing to the stock waitlist for {product}. We will email you once the product back in stock.', 'merchant' ),
 			'desc'    => esc_html__( 'The message that will be sent to new subscribers.', 'merchant' ),
 		),
 
@@ -70,7 +70,7 @@ Merchant_Admin_Options::create( array(
 			'id'      => 'email_update',
 			'type'    => 'textarea',
 			'title'   => esc_html__( 'Email in stock update', 'merchant' ),
-			'default' => __( 'Hello Subscriber, Thanks for your patience and finally the wait is over! Your Subscribed Product {product} is now back in stock! We only have a limited amount of stock, and this email is not a guarantee you\'ll get one, so hurry to be one of the lucky shoppers who do. Add this product {product} directly to your cart.',
+			'default' => __( 'Hello, thanks for your patience and finally the wait is over! Your {product} is now back in stock! We only have a limited amount of stock, and this email is not a guarantee you’ll get one. Add this {product} directly to your cart.',
 				'merchant' ),
 			'desc'    => esc_html__( 'The message that will be sent to subscribers when product is in stock.', 'merchant' ),
 		),
