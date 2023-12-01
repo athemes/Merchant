66dfcdd89a50a46e1d785cf11d39b74512214f08
devwael <dev.ahmedwael@gmail.com>
2023-12-01 17:00:57 +0200

    Add translation for free gifts module

--- inc/modules/free-gifts/class-free-gifts.php
+++ inc/modules/free-gifts/class-free-gifts.php
@@ -90,6 +90,32 @@ class Merchant_Free_Gifts extends Merchant_Add_Module {
 			// The custom CSS should be added here as well due to ensure preview box works properly.
 			add_filter( 'merchant_custom_css', array( $this, 'admin_custom_css' ) );
 		}
+
+		if ( Merchant_Modules::is_module_active( self::MODULE_ID ) ) {
+			// Init translations.
+			$this->init_translations();
+		}
+	}
+
+	/**
+	 * Init translations.
+	 *
+	 * @return void
+	 */
+	public function init_translations() {
+		$settings = $this->get_module_settings();
+		if ( ! empty( $settings['spending_text'] ) ) {
+			Merchant_Translator::register_string( $settings['spending_text'], esc_html__( 'Free Gifts: Spending text', 'merchant' ) );
+		}
+		if ( ! empty( $settings['free_text'] ) ) {
+			Merchant_Translator::register_string( $settings['free_text'], esc_html__( 'Free Gifts: Free text', 'merchant' ) );
+		}
+		if ( ! empty( $settings['cart_title_text'] ) ) {
+			Merchant_Translator::register_string( $settings['cart_title_text'], esc_html__( 'Free Gifts: Cart item title text', 'merchant' ) );
+		}
+		if ( ! empty( $settings['cart_description_text'] ) ) {
+			Merchant_Translator::register_string( $settings['cart_description_text'], esc_html__( 'Free Gifts: Cart item description text', 'merchant' ) );
+		}
 	}
 
 	/**
