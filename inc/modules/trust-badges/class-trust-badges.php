5aa076c3c3e49ed37af99455a434a0ae05c79a97
devwael <dev.ahmedwael@gmail.com>
2023-12-07 19:39:21 +0200

    Fix filter params

--- inc/modules/trust-badges/class-trust-badges.php
+++ inc/modules/trust-badges/class-trust-badges.php
@@ -183,7 +183,7 @@ class Merchant_Trust_Badges extends Merchant_Add_Module {
 		 *
 		 * @since 1.7
 		 */
-		return apply_filters( 'merchant_module_shortcode_content_html', $shortcode_content, $this->module->module_id, get_the_ID() );
+		return apply_filters( 'merchant_module_shortcode_content_html', $shortcode_content, $this->module_id, get_the_ID() );
 	}
 
 	/**
