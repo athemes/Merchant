8c52aa1c7983443daaf9a5ec6675a5292352e929
devwael <dev.ahmedwael@gmail.com>
2023-12-13 17:24:27 +0200

    Add support for extra URL params

--- inc/modules/address-autocomplete/admin/options.php
+++ inc/modules/address-autocomplete/admin/options.php
@@ -16,10 +16,16 @@ Merchant_Admin_Options::create( array(
 	'title'  => esc_html__( 'Settings', 'merchant' ),
 	'fields' => array(
 		array(
-			'id'      => 'api_key',
-			'type'    => 'text',
-			'title'   => esc_html__( 'API Key', 'merchant' ),
-			'desc'    => esc_html__( 'Add Google places API Key', 'merchant' ),
+			'id'    => 'api_key',
+			'type'  => 'text',
+			'title' => esc_html__( 'API Key', 'merchant' ),
+			'desc'  => esc_html__( 'Add Google places API Key', 'merchant' ),
+		),
+		array(
+			'id'    => 'url_params',
+			'type'  => 'text',
+			'title' => esc_html__( 'Optional API URL Parameters', 'merchant' ),
+			'desc'  => esc_html__( 'Add extra parameters to the API URL. For example: &region=us&language=en.', 'merchant' ),
 		),
 	),
 ) );
