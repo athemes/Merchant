bd927ae38075828fd9424479b1a2132161fb650b
devwael <dev.ahmedwael@gmail.com>
2023-12-18 20:25:50 +0200

    Display message for checkout block

--- inc/modules/address-autocomplete/admin/options.php
+++ inc/modules/address-autocomplete/admin/options.php
@@ -10,11 +10,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
+$before_fields = array();
+if ( defined( 'MERCHANT_PRO_VERSION' ) && merchant_is_checkout_block_layout() ) {
+	$before_fields = array(
+		'type'    => 'warning',
+		'content' => sprintf(
+		/* Translators: 1. docs link */
+			__( 'Your checkout page is being rendered through the new WooCommerce checkout block. You must edit the checkout page to use the classic checkout shortcode instead. Check <a href="%1$s" target="_blank">this documentation</a> to learn more.', 'merchant' ),
+			'https://docs.athemes.com/article/how-to-switch-cart-checkout-blocks-to-the-classic-shortcodes/'
+		),
+	);
+}
 // Settings
 Merchant_Admin_Options::create( array(
 	'module' => Merchant_Address_Autocomplete::MODULE_ID,
 	'title'  => esc_html__( 'Settings', 'merchant' ),
 	'fields' => array(
+		$before_fields,
 		array(
 			'id'    => 'api_key',
 			'type'  => 'text',
