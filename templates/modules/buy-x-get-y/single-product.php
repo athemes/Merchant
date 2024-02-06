2b0dcaa95e1e64638ce049bc44950b929302411d
devwael <dev.ahmedwael@gmail.com>
2024-02-06 22:26:23 +0200

    Adjust module frontend template to match new data

--- templates/modules/buy-x-get-y/single-product.php
+++ templates/modules/buy-x-get-y/single-product.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $settings = isset( $args['settings'] ) ? $args['settings'] : array();
-$product  = isset( $args['product'] ) ? wc_get_product( $args['product'] ) : wc_get_product();
+$product  = isset( $args['product'] ) ? Merchant_Pro_Buy_X_Get_Y::product_args( wc_get_product( $args['product'] ) ) : wc_get_product();
 ?>
 <div class="merchant-bogo">
     <p class="merchant-bogo-title">
@@ -25,11 +25,12 @@ $product  = isset( $args['product'] ) ? wc_get_product( $args['product'] ) : wc_
 		<?php
 		foreach ( $args['offers'] as $key => $offer ): ?>
 			<?php
-			if ( isset( $offer['product']['is_purchasable'] ) && ! $offer['product']['is_purchasable'] ) {
+			$buy_product = $offer['customer_get_product_ids'] ? Merchant_Pro_Buy_X_Get_Y::product_args( wc_get_product( $offer['customer_get_product_ids'] ) ) : null;
+			if ( isset( $product['is_purchasable'] ) && ! $product['is_purchasable'] ) {
 				continue;
 			} ?>
             <div class="merchant-bogo-offer" data-product="<?php
-			echo esc_attr( get_the_ID() ) ?>" data-offer="<?php
+			echo esc_attr( $product['id'] ) ?>" data-offer="<?php
 			echo esc_attr( $key ); ?>">
                 <div class="merchant-bogo-product-x">
                     <div class="merchant-bogo-product-label merchant-bogo-product-buy-label">
@@ -43,24 +44,24 @@ $product  = isset( $args['product'] ) ? wc_get_product( $args['product'] ) : wc_
                     </div>
                     <div class="merchant-bogo-product">
 						<?php
-						echo wp_kses_post( $product->get_image( array( 90, 90 ) ) ); ?>
+						echo wp_kses_post( $product['image'] ); ?>
                         <div class="merchant-bogo-product-contents">
                             <p class="woocommerce-loop-product__title">
                                 <a href="<?php
-								echo esc_url( $product->get_permalink() ); ?>" target="_blank">
+								echo esc_url( $product['permalink'] ); ?>" target="_blank">
 									<?php
-									echo esc_html( $product->get_name() ); ?>
+									echo esc_html( $product['title'] ); ?>
                                 </a>
                             </p>
 							<?php
-							echo wp_kses( $product->get_price_html(), merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
+							echo wp_kses( $product['price_html'], merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
                         </div>
                     </div>
                     <div class="merchant-bogo-arrow">→</div>
                 </div>
                 <div class="merchant-bogo-product-y">
                     <form class="merchant-bogo-form" data-product="<?php
-					echo esc_attr( $product->get_id() ); ?>">
+					echo esc_attr( $buy_product['id'] ); ?>">
                         <div class="merchant-bogo-product-label merchant-bogo-product-get-label">
 							<?php
 							$discount = $offer['discount_type'] === 'percentage'
@@ -85,34 +86,33 @@ $product  = isset( $args['product'] ) ? wc_get_product( $args['product'] ) : wc_
 								); ?>
                         </div>
 						<?php
-						$buy_product = $offer['customer_get_product_ids'] ? wc_get_product( $offer['customer_get_product_ids'] ) : null;
 						if ( $buy_product ) {
 							?>
                             <div class="merchant-bogo-product">
 								<?php
-								echo wp_kses_post( $buy_product->get_image( array( 90, 90 ) ) ); ?>
+								echo wp_kses_post( $buy_product['image'] ); ?>
                                 <div class="merchant-bogo-product-contents">
                                     <p class="woocommerce-loop-product__title">
                                         <a href="<?php
-										echo esc_url( $buy_product->get_permalink() ); ?>" target="_blank">
+										echo esc_url( $buy_product['permalink'] ); ?>" target="_blank">
 											<?php
-											echo esc_html( $buy_product->get_name() ); ?>
+											echo esc_html( $buy_product['title'] ); ?>
                                         </a>
                                     </p>
                                     <div class="merchant-bogo-product-price">
 										<?php
-										echo wp_kses( $buy_product->get_price_html(), merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
+										echo wp_kses( $buy_product['price_html'], merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
                                     </div>
                                 </div>
                             </div>
 							<?php
-							if ( isset( $offer['product']['attributes'] ) && ! empty( $offer['product']['attributes'] ) ) : ?>
+							if ( isset( $buy_product['attributes'] ) && ! empty( $buy_product['attributes'] ) ) : ?>
                                 <div class="merchant-bogo-product-attributes" data-nonce="<?php
 								echo esc_attr( wp_create_nonce( 'mrc_get_variation_data_nonce' ) ); ?>">
 									<?php
-									foreach ( $offer['product']['attributes'] as $key => $attribute ) : ?>
+									foreach ( $buy_product['attributes'] as $buy_product_key => $attribute ) : ?>
                                         <select class="merchant-bogo-select-attribute" name="<?php
-										echo esc_attr( $key ) ?>" required>
+										echo esc_attr( $buy_product_key ) ?>" required>
                                             <option value="">
 												<?php
 												echo esc_html(
@@ -139,7 +139,7 @@ $product  = isset( $args['product'] ) ? wc_get_product( $args['product'] ) : wc_
 									: esc_html__( 'Add To Cart', 'merchant' ); ?>
                             </button>
                             <div class="merchant-bogo-offer-error"></div>
-						<?php
+							<?php
 						} ?>
                     </form>
                 </div>
