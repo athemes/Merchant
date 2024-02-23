018f2e40a0a55e4b4da8317d3bfb0e8a326e7558
devwael <dev.ahmedwael@gmail.com>
2024-02-23 22:15:15 +0200

    Add new field wc coupon selector

--- admin/classes/class-merchant-admin-options.php
+++ admin/classes/class-merchant-admin-options.php
@@ -1511,6 +1511,51 @@ if ( ! class_exists( 'Merchant_Admin_Options' ) ) {
 			<?php
 		}
 
+		public static function wc_coupons( $settings, $value ) {
+			$coupons = get_posts( array(
+				'posts_per_page' => - 1,
+				'orderby'        => 'title',
+				'order'          => 'asc',
+				'post_type'      => 'shop_coupon',
+				'post_status'    => 'publish',
+			) );
+			if ( $coupons ) {
+                ?>
+                    <div class="merchant-wc-coupon-selector merchant-<?php
+				echo esc_attr( $settings['id'] ) ?>">
+				<?php
+				echo '<select name="merchant[' . esc_attr( $settings['id'] ) . ']">';
+				echo '<option value="">' . esc_html__( 'Select a coupon', 'merchant' ) . '</option>';
+
+				foreach ( $coupons as $coupon ) {
+					$coupon_code = strtolower( $coupon->post_title );
+
+					echo '<option value="' . esc_attr( $coupon_code ) . '"' . selected( esc_attr( $coupon_code ), $value, false ) . '>';
+					echo esc_html( $coupon_code );
+					echo '</option>';
+				}
+
+				echo '</select>';
+				echo '<a href="' . esc_url( admin_url( 'edit.php?post_type=shop_coupon' ) ) . '" target="_blank" style="margin-left: 10px;">' . esc_html__( 'Manage coupons',
+						'merchant' ) . '</a>';
+				?>
+                    </div>
+				<?php
+			} else {
+				echo '<div style="color: red;">';
+				echo wp_kses_post(
+					sprintf(
+					/* Translators: 1. Coupon admin url 2. Link target attribute value */
+						__( 'No coupons found! <a href="%1$s" target="%2$s">Create a new coupon</a>', 'merchant' ),
+						admin_url( 'post-new.php?post_type=shop_coupon' ),
+						'_blank'
+					)
+				);
+				echo '</div>';
+				echo '<input type="hidden" name="merchant[' . esc_attr( $settings['id'] ) . ']" value="" />';
+			}
+		}
+
 		/**
 		 * Field: Flexible Content.
 		 *
