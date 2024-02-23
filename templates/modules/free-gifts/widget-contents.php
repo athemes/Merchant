620fb7da57b174d2fbcf8f0261ea14df6efeb299
devwael <dev.ahmedwael@gmail.com>
2024-02-23 22:16:42 +0200

    Update free gifts module frontend widget

--- templates/modules/free-gifts/widget-contents.php
+++ templates/modules/free-gifts/widget-contents.php
@@ -15,7 +15,8 @@ $settings = isset( $args['settings'] ) ? $args['settings'] : array();
 ?>
 
 <?php foreach ( $args['offers'] as $offer ): ?>
-	<?php if ( $args['cart_total'] < $offer['amount'] ) : ?>
+	<?php
+	if ( isset( $offer['amount'], $args['cart_total'] ) && $args['cart_total'] < $offer['amount'] ) : ?>
         <div class="merchant-free-gifts-widget-offer">
             <div class="merchant-free-gifts-widget-offer-label">
 				<?php
@@ -26,11 +27,50 @@ $settings = isset( $args['settings'] ) ? $args['settings'] : array();
 						$amount,
 						sanitize_text_field( Merchant_Translator::translate( $settings['spending_text'] ) )
                     ), merchant_kses_allowed_tags( array( 'bdi' ) ) )
-					: wp_kses( 
+					: wp_kses(
                         /* Translators: 1. Amount */
                         sprintf( __( 'Spend %s more to receive this free gift!', 'merchant' ), $amount ),
                         merchant_kses_allowed_tags( array( 'bdi' ) )
                     ); ?>
+            </div>
+            <div class="merchant-free-gifts-widget-offer-product">
+				<?php echo wp_kses_post( $offer['product']['image'] ); ?>
+                <div class="merchant-free-gifts-widget-offer-product-contents">
+                    <p class="woocommerce-loop-product__title">
+                        <a class="merchant-free-gifts-widget-offer-product-title" href="<?php echo esc_url( $offer['product']['permalink'] ); ?>" target="_blank">
+							<?php echo esc_html( $offer['product']['title'] ); ?>
+                        </a>
+                    </p>
+                    <div class="merchant-free-gifts-widget-offer-product-price price">
+                        <del>
+							<?php echo wp_kses( wc_price( $offer['product']['price'] ), merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
+                        </del>
+                        <strong class="merchant-free-gifts-widget-offer-product-free">
+							<?php echo isset( $settings['free_text'] ) ? esc_html( Merchant_Translator::translate( $settings['free_text'] ) ) : esc_html__( 'Free', 'merchant' ) ?>
+                        </strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+	<?php else:
+		if ( isset( $offer['coupon'] ) && ! empty( $offer['coupon'] ) ) {
+			$coupon = $offer['coupon'];
+		} else {
+            continue;
+        }
+		$applied_coupons = WC()->cart->get_applied_coupons();
+		if ( in_array( $coupon, $applied_coupons, true ) ) {
+			continue;
+		}
+		?>
+        <div class="merchant-free-gifts-widget-offer">
+            <div class="merchant-free-gifts-widget-offer-label">
+				<?php
+				echo wp_kses(
+					/* Translators: 1. Amount */
+						sprintf( __( 'Use %s coupon to get this product', 'merchant' ), $coupon ),
+						merchant_kses_allowed_tags( array( 'bdi' ) )
+					); ?>
             </div>
             <div class="merchant-free-gifts-widget-offer-product">
 				<?php echo wp_kses_post( $offer['product']['image'] ); ?>
