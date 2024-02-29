1fe827ed82ea566954bed78b3027dc36a4e53f1a
devwael <dev.ahmedwael@gmail.com>
2024-02-29 21:52:28 +0200

    Free Gifts: update widget to support product based rule

--- templates/modules/free-gifts/widget-contents.php
+++ templates/modules/free-gifts/widget-contents.php
@@ -16,7 +16,40 @@ $settings = isset( $args['settings'] ) ? $args['settings'] : array();
 
 <?php foreach ( $args['offers'] as $offer ): ?>
 	<?php
-	if ( isset( $offer['amount'], $args['cart_total'] ) && $args['cart_total'] < $offer['amount'] ) : ?>
+	if ( isset( $offer['rules_to_display'], $offer['product_to_purchase'] )
+        && 'product' === $offer['rules_to_display']
+		&& ! merchant_is_already_in_cart( $offer['product_to_purchase'] )
+	) :
+		$product = wc_get_product( $offer['product_to_purchase'] );
+		?>
+        <div class="merchant-free-gifts-widget-offer">
+            <div class="merchant-free-gifts-widget-offer-label">
+				<?php echo wp_kses(
+				/* Translators: 1. Amount */
+					sprintf( __( 'Buy %s to receive this free gift!', 'merchant' ), $product->get_title() ),
+					merchant_kses_allowed_tags( array( 'bdi' ) )
+				); ?>
+            </div>
+            <div class="merchant-free-gifts-widget-offer-product">
+				<?php echo wp_kses_post( $offer['product']['image'] ); ?>
+                <div class="merchant-free-gifts-widget-offer-product-contents">
+                    <p class="woocommerce-loop-product__title">
+                        <a class="merchant-free-gifts-widget-offer-product-title" href="<?php echo esc_url( $offer['product']['permalink'] ); ?>" target="_blank">
+							<?php echo esc_html( $offer['product']['title'] ); ?>
+                        </a>
+                    </p>
+                    <div class="merchant-free-gifts-widget-offer-product-price price">
+                        <del>
+							<?php echo wp_kses( wc_price( $offer['product']['price'] ), merchant_kses_allowed_tags( array( 'bdi' ) ) ); ?>
+                        </del>
+                        <strong class="merchant-free-gifts-widget-offer-product-free">
+							<?php echo isset( $settings['free_text'] ) ? esc_html( Merchant_Translator::translate( $settings['free_text'] ) ) : esc_html__( 'Free', 'merchant' ) ?>
+                        </strong>
+                    </div>
+                </div>
+            </div>
+        </div>
+	<?php elseif ( isset( $offer['amount'], $args['cart_total'] ) && $args['cart_total'] < $offer['amount'] ) : ?>
         <div class="merchant-free-gifts-widget-offer">
             <div class="merchant-free-gifts-widget-offer-label">
 				<?php
