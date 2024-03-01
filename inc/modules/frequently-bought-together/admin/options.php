a27a7e276fbf5251124edc189dd5132bf75f4fe9
devwael <dev.ahmedwael@gmail.com>
2024-03-01 16:47:59 +0200

    FBT: Update copy

--- inc/modules/frequently-bought-together/admin/options.php
+++ inc/modules/frequently-bought-together/admin/options.php
@@ -79,7 +79,7 @@ Merchant_Admin_Options::create( array(
 						array(
 							'id'      => 'enable_discount',
 							'type'    => 'switcher',
-							'title'   => __( 'Do you want to offer a discount on this bundle?', 'merchant' ),
+							'title'   => __( 'Offer a discount on this bundle', 'merchant' ),
 							'default' => 0,
 						),
 						array(
