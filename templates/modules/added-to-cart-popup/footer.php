3d7fb8b344fab1302f2441b6738dc8b6e31c1a52
devwael <dev.ahmedwael@gmail.com>
2024-04-04 21:32:38 +0200

    Set chosen layout

--- templates/modules/added-to-cart-popup/footer.php
+++ templates/modules/added-to-cart-popup/footer.php
@@ -14,9 +14,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 </div>
 <div class="merchant-hidden-popup-structure">
 	<?php
+	/**
+	 * Filter the allowed added to cart popup layouts.
+	 *
+	 * @param array $allowed_layouts The allowed layouts.
+	 *
+	 * @since 1.9.6
+	 */
+	$allowed_layouts = apply_filters( 'merchant_added_to_cart_popup_allowed_layouts', array( 'layout-1', 'layout-2', 'layout-3' ) );
+	$layout          = 'layout-1';
+    $settings = $args['settings'];
+	if ( isset( $settings['layout'] ) && in_array( $settings['layout'], $allowed_layouts, true ) ) {
+		$layout = $settings['layout'];
+	}
 	merchant_get_template_part(
 		'modules/added-to-cart-popup/layouts',
-		'layout-1',
+		/**
+		 * Filter the added to cart popup layout.
+		 *
+		 * @param string $layout The layout name.
+		 *
+		 * @return string The layout name.
+		 *
+		 * @since 1.9.6
+		 */
+		apply_filters( 'merchant_added_to_cart_popup_layout', $layout ),
 		$args
 	);
 	?>
