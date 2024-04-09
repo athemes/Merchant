c404737cffc9099a21f23e4cafd85b3b5bad3090
devwael <dev.ahmedwael@gmail.com>
2024-04-09 02:26:50 +0200

    Make action buttons toggleable

--- templates/modules/added-to-cart-popup/common/action-buttons.php
+++ templates/modules/added-to-cart-popup/common/action-buttons.php
@@ -1,21 +1,41 @@
+<?php
+/**
+ * Template for added to cart popup action buttons.
+ *
+ * @var $args array template args
+ *
+ * @since 1.9.7
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+if (
+	! isset( $args['settings']['show_view_cart_button'] )
+	|| ! isset( $args['settings']['show_checkout_button'] )
+	|| ! isset( $args['settings']['show_view_continue_shopping_button'] )
+) {
+	return;
+}
+?>
 <div class="popup-actions">
 	<?php
 	// View cart button.
 	if ( isset( $args['settings']['show_view_cart_button'] ) && $args['settings']['show_view_cart_button'] ) { ?>
-		<a href="<?php
+        <a href="<?php
 		echo esc_url( wc_get_cart_url() ); ?>" class="merchant-button button-filled view-cart">
 			<?php
 			echo $args['settings']['view_cart_button_label']
 				? esc_html( Merchant_Translator::translate( $args['settings']['view_cart_button_label'] ) )
 				: esc_html__
 				( 'View Cart', 'merchant' ); ?>
-		</a>
+        </a>
 		<?php
 	} ?>
 	<?php
 	// Checkout button.
 	if ( isset( $args['settings']['show_checkout_button'] ) && $args['settings']['show_checkout_button'] ) { ?>
-		<a href="<?php
+        <a href="<?php
 		echo esc_url( wc_get_checkout_url() ); ?>" class="merchant-button checkout"><?php
 			esc_html_e( 'Checkout', 'merchant' ); ?></a>
 		<?php
@@ -23,12 +43,12 @@
 	<?php
 	// Continue shopping button.
 	if ( isset( $args['settings']['show_view_continue_shopping_button'] ) && $args['settings']['show_view_continue_shopping_button'] ) { ?>
-		<a href="#" class="merchant-button continue-shopping popup-close-js">
+        <a href="#" class="merchant-button continue-shopping popup-close-js">
 			<?php
 			echo $args['settings']['view_continue_shopping_button_label']
 				? esc_html( Merchant_Translator::translate( $args['settings']['view_continue_shopping_button_label'] ) )
 				: esc_html__( 'Continue Shopping', 'merchant' ); ?>
-		</a>
+        </a>
 		<?php
 	} ?>
 </div>
