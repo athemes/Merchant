6204d3f3677ac0afa91b0f83ef04229bda95ead0
Nahid <nahiddhaka07@gmail.com>
2024-04-16 20:09:07 +0600

    Fix fatal error

--- inc/classes/class-merchant-modules.php
+++ inc/classes/class-merchant-modules.php
@@ -219,6 +219,10 @@ if ( ! class_exists( 'Merchant_Modules' ) ) {
 			// Preview Mode
 			$operation_mode = Merchant_Option::get( 'global-settings', 'operating_mode', 'active' );
 
+			if ( ! function_exists( 'wp_get_current_user' ) ) {
+				include( ABSPATH . 'wp-includes/pluggable.php' );
+			}
+
 			if ( 'inactive' === $operation_mode ) {
 				return false;
 			} elseif ( 'preview' === $operation_mode && ! current_user_can( 'manage_options' ) ) {
