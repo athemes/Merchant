61c3b709ea60de917b8dc46281ab60d23cdc45cd
devwael <dev.ahmedwael@gmail.com>
2024-04-16 22:38:12 +0200

    Adjust mobile view content for FBT content

--- templates/modules/added-to-cart-popup/common/frequently-bought-together.php
+++ templates/modules/added-to-cart-popup/common/frequently-bought-together.php
@@ -20,7 +20,7 @@ foreach ( $args['product_offers'] as $offer ) {
 			echo esc_html( Merchant_Translator::translate( $offer['title'] ) ); ?></h3>
         <div class="offer-products">
             <div class="offer-column">
-                <div class="offer-product">
+                <div class="offer-product main-product">
                     <div class="image-wrapper">
                         <a href="<?php
 						echo esc_url( $args['popup_data']['product_url'] ); ?>">
@@ -38,14 +38,15 @@ foreach ( $args['product_offers'] as $offer ) {
                     </div>
                 </div>
             </div>
-            <div class="offer-column half-width">
+            <div class="offer-column half-width computer-only">
                 <div class="offer-icon">
                     <span class="plus-icon">+</span>
                 </div>
             </div>
-            <div class="offer-column wide">
+            <div class="offer-column slider-area computer-only">
                 <div class="products-slider-container">
-                    <div class="products-slider<?php echo count($offer['products']) > 1 ? esc_attr(' multiple-slides') : '';?>">
+                    <div class="products-slider<?php
+					echo count( $offer['products'] ) > 1 ? esc_attr( ' multiple-slides' ) : ''; ?>">
 						<?php
 						foreach ( $offer['products'] as $product_data ) { ?>
                             <div class="offer-product">
@@ -68,6 +69,34 @@ foreach ( $args['product_offers'] as $offer ) {
 							<?php
 						} ?>
                     </div>
+                </div>
+            </div>
+            <div class="offer-column mobile-only">
+                <div class="offer-products">
+					<?php
+					foreach ( $offer['products'] as $product_data ) { ?>
+                        <div class="offer-product other-products">
+                            <div class="offer-icon">
+                                <span class="plus-icon">+</span>
+                            </div>
+                            <div class="image-wrapper">
+                                <a href="<?php
+								echo esc_url( $product_data['permalink'] ); ?>">
+									<?php
+									echo wp_kses_post( $product_data['image_big'] ); ?>
+                                </a>
+                            </div>
+                            <div class="product-summary">
+                                <a href="<?php
+								echo esc_url( $product_data['permalink'] ); ?>">
+                                    <h3><?php
+										echo esc_html( $product_data['title'] ); ?></h3></a>
+                                <div class="product-price"><?php
+									echo wp_kses_post( $product_data['price_html'] ); ?></div>
+                            </div>
+                        </div>
+						<?php
+					} ?>
                 </div>
             </div>
             <div class="offer-column narrow">
