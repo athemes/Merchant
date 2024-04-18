0a9f197c5cd0fbb929fab9746424a247dd4c117a
Nahid <nahiddhaka07@gmail.com>
2024-04-19 02:01:40 +0600

    Improve logic

--- assets/js/src/modules/buy-now/buy-now.js
+++ assets/js/src/modules/buy-now/buy-now.js
@@ -1,12 +1,17 @@
 ;( function( $ ) {
 	$( document ).ready( function() {
 
-		// Update buy now button state based on selected variation
-		$( 'form.variations_form' ).on( 'woocommerce_variation_has_changed', function() {
-			const $form = $( this );
-			const selectedVariationId = $form.find( '.variation_id' ).val();
-			$form.find( '.merchant-buy-now-button' ).toggleClass( 'disabled', ! selectedVariationId );
-		} );
+		const $variationForm = $( 'form.variations_form' );
 
+		// Update buy now button state based on selected variation
+		$variationForm.each( function () {
+			const $form = $( this );
+			const $buyBtn = $form.find( '.merchant-buy-now-button' );
+
+			$form.find( 'input[name="variation_id"]' ).on( 'change woocommerce_variation_has_changed', function () {
+				const selectedVariationId = +$form.find( '.variation_id' ).val();
+				$buyBtn.toggleClass( 'disabled', ! selectedVariationId );
+			} )
+		} );
 	} );
 } )( jQuery );
