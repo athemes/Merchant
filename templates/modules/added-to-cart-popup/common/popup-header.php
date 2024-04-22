5799d12950f3a08f62077be0eef8e31411ffe115
devwael <dev.ahmedwael@gmail.com>
2024-04-22 19:30:25 +0200

    Load default settings

--- templates/modules/added-to-cart-popup/common/popup-header.php
+++ templates/modules/added-to-cart-popup/common/popup-header.php
@@ -10,12 +10,12 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+$header_title       = Merchant_Admin_Options::get( 'added-to-cart-popup', 'popup_message', esc_html__( 'Added to Cart', 'merchant' ) );
+$close_button_color = Merchant_Admin_Options::get( 'added-to-cart-popup', 'close_btn_color', '#000000' );
 ?>
 <div class="popup-header">
     <h3 class="popup-header-title"><?php
-		echo ! empty( $args['settings']['popup_message'] )
-			? esc_html( Merchant_Translator::translate( $args['settings']['popup_message'] ) )
-			: esc_html__( 'Added to Cart', 'merchant' ); ?>
+		echo esc_html( Merchant_Translator::translate( $header_title ) ); ?>
     </h3>
     <div class="popup-close">
                 <span class="close-button popup-close-js" title="<?php
@@ -24,7 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
                         <path
                                 d="M10.333 1.43359L1.73047 10.0361M1.73047 1.43359L10.333 10.0361"
                                 stroke="<?php
-		                        echo esc_attr( isset( $args['settings']['close_btn_color'] ) ? $args['settings']['close_btn_color'] : '#000000' ); ?>"
+		                        echo esc_attr( $close_button_color ); ?>"
                                 stroke-width="1.5"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
