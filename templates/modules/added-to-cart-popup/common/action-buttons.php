9797bd53ca953411febbac7982a3d943204d208a
devwael <dev.ahmedwael@gmail.com>
2024-04-22 19:30:19 +0200

    Load default settings

--- templates/modules/added-to-cart-popup/common/action-buttons.php
+++ templates/modules/added-to-cart-popup/common/action-buttons.php
@@ -10,10 +10,15 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
+$view_cart_toggle         = Merchant_Admin_Options::get( 'added-to-cart-popup', 'show_view_cart_button', true );
+$checkout_toggle          = Merchant_Admin_Options::get( 'added-to-cart-popup', 'show_checkout_button', true );
+$continue_shopping_toggle = Merchant_Admin_Options::get( 'added-to-cart-popup', 'show_view_continue_shopping_button', true );
+$view_cart_label          = Merchant_Admin_Options::get( 'added-to-cart-popup', 'view_cart_button_label', __( 'View Cart', 'merchant' ) );
+$continue_shopping_label  = Merchant_Admin_Options::get( 'added-to-cart-popup', 'view_continue_shopping_button_label', __( 'Continue Shopping', 'merchant' ) );
 if (
-	! isset( $args['settings']['show_view_cart_button'] )
-	|| ! isset( $args['settings']['show_checkout_button'] )
-	|| ! isset( $args['settings']['show_view_continue_shopping_button'] )
+	! $view_cart_toggle
+	&& ! $checkout_toggle
+	&& ! $continue_shopping_toggle
 ) {
 	return;
 }
@@ -21,31 +26,28 @@ if (
 <div class="popup-actions">
 	<?php
 	// View cart button.
-	if ( isset( $args['settings']['show_view_cart_button'] ) && $args['settings']['show_view_cart_button'] ) { ?>
+	if ( $view_cart_toggle ) { ?>
         <a href="<?php
 		echo esc_url( wc_get_cart_url() ); ?>" class="merchant-button button-filled view-cart">
 			<?php
-			echo $args['settings']['view_cart_button_label']
-				? esc_html( Merchant_Translator::translate( $args['settings']['view_cart_button_label'] ) )
-				: esc_html__
-				( 'View Cart', 'merchant' ); ?>
+			echo
+			esc_html( Merchant_Translator::translate( $view_cart_label ) ); ?>
         </a>
 		<?php
 	} ?>
 	<?php
 	// Continue shopping button.
-	if ( isset( $args['settings']['show_view_continue_shopping_button'] ) && $args['settings']['show_view_continue_shopping_button'] ) { ?>
+	if ( $continue_shopping_toggle ) { ?>
         <a href="#" class="merchant-button continue-shopping popup-close-js">
 			<?php
-			echo $args['settings']['view_continue_shopping_button_label']
-				? esc_html( Merchant_Translator::translate( $args['settings']['view_continue_shopping_button_label'] ) )
-				: esc_html__( 'Continue Shopping', 'merchant' ); ?>
+			echo
+			esc_html( Merchant_Translator::translate( $continue_shopping_label ) ); ?>
         </a>
 		<?php
 	} ?>
 	<?php
 	// Checkout button.
-	if ( isset( $args['settings']['show_checkout_button'] ) && $args['settings']['show_checkout_button'] ) { ?>
+	if ( $checkout_toggle ) { ?>
         <a href="<?php
 		echo esc_url( wc_get_checkout_url() ); ?>" class="merchant-button checkout"><?php
 			esc_html_e( 'Checkout', 'merchant' ); ?></a>
