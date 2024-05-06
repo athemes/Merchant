cc1062fdfb3bb865afe9aad9187e02d4c0fd4133
Nahid <nahiddhaka07@gmail.com>
2024-05-06 22:10:24 +0600

    work continues

--- templates/modules/advanced-reviews/photo-slider.php
+++ templates/modules/advanced-reviews/photo-slider.php
@@ -1,3 +1,19 @@
-<?php
 
-echo 'Hello';
+<div class="merchant-adv-reviews-modal show">
+	<div class="merchant-adv-reviews-modal-body">
+		<button>Close</button>
+		<div class="merchant-adv-reviews-modal-content">
+			<div class="merchant-adv-reviews-modal-column">
+				<div class="photo">
+					<img src="http://at.local/wp-content/uploads/2024/05/5-11-1024x683.jpg" alt="">
+				</div>
+			</div>
+			<div class="merchant-adv-reviews-modal-column">
+				<h2>Title</h2>
+				<div>Rating</div>
+				<p>Name</p>
+				<p>Comment</p>
+			</div>
+		</div>
+	</div>
+</div>
