0b1a2a50c57f355a0985dd41b12685fd969ad4e3
devwael <dev.ahmedwael@gmail.com>
2024-05-16 18:25:30 +0300

    Build multiple rules in pre-orders

--- inc/modules/pre-orders/admin/options.php
+++ inc/modules/pre-orders/admin/options.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Pre Orders.
- * 
+ *
  * @package Merchant
  */
 
@@ -9,76 +9,286 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-/**
- * Settings
- */
-Merchant_Admin_Options::create( array(
-	'title'  => esc_html__( 'Settings', 'merchant' ),
-	'module' => 'pre-orders',
-	'fields' => array(
 
-		array(
-			'id'      => 'button_text',
-			'type'    => 'text',
-			'title'   => esc_html__( 'Button text', 'merchant' ),
-			'default' => esc_html__( 'Pre Order Now!', 'merchant' ),
+Merchant_Admin_Options::create(
+	array(
+		'title'  => esc_html__( 'Pre-order Rule', 'merchant' ),
+		'module' => Merchant_Pre_Orders::MODULE_ID,
+		'fields' => array(
+
+			array(
+				'id'           => 'rules',
+				'type'         => 'flexible_content',
+				'sorting'      => true,
+				'accordion'    => true,
+				'style'        => Merchant_Pre_Orders::MODULE_ID . '-style default',
+				'button_label' => esc_html__( 'Add New Offer', 'merchant' ),
+				'layouts'      => array(
+					'rule-details' => array(
+						'title'       => esc_html__( 'Campaign', 'merchant' ),
+						'title-field' => 'offer-title', // text field ID to use as title for the layout
+						'fields'      => array(
+							array(
+								'id'      => 'offer-title',
+								'type'    => 'text',
+								'title'   => esc_html__( 'Order name', 'merchant' ),
+								'default' => esc_html__( 'Custom Pre-order', 'merchant' ),
+								'desc'    => esc_html__( 'Internal campaign name. This is not visible to customers.', 'merchant' ),
+							),
+							array(
+								'id'      => 'trigger_on',
+								'type'    => 'select',
+								'title'   => esc_html__( 'Trigger', 'merchant' ),
+								'options' => array(
+									'product'  => esc_html__( 'Specific product', 'merchant' ),
+									'category' => esc_html__( 'Specific category', 'merchant' ),
+								),
+								'default' => 'product',
+							),
+							array(
+								'id'        => 'product_ids',
+								'type'      => 'products_selector',
+								'multiple'  => true,
+								'desc'      => esc_html__( 'Select the product(s) included in this pre-order.', 'merchant' ),
+								'condition' => array( 'trigger_on', '==', 'product' ),
+							),
+							array(
+								'id'          => 'category_slugs',
+								'type'        => 'select_ajax',
+								'title'       => esc_html__( 'Categories', 'merchant' ),
+								'source'      => 'options',
+								'multiple'    => true,
+								'options'     => Merchant_Admin_Options::get_category_select2_choices(),
+								'placeholder' => esc_html__( 'Select categories', 'merchant' ),
+								'desc'        => esc_html__( 'Select the product categories that will show the offer.', 'merchant' ),
+								'condition'   => array( 'trigger_on', '==', 'category' ),
+							),
+							array(
+								'id'      => 'discount_toggle',
+								'type'    => 'switcher',
+								'title'   => __( 'Do you want to offer a discount on this pre-order?', 'merchant' ),
+								'default' => 0,
+							),
+							array(
+								'id'         => 'discount_type',
+								'type'       => 'radio',
+								'title'      => esc_html__( 'Discount Type', 'merchant' ),
+								'options'    => array(
+									'percentage' => esc_html__( 'Percentage', 'merchant' ),
+									'fixed'      => esc_html__( 'Fixed', 'merchant' ),
+								),
+								'default'    => 'percentage',
+								'conditions' => array(
+									'terms' => array(
+										array(
+											'field'    => 'discount_toggle', // field ID
+											'operator' => '===', // Available operators: ===, !==, >, <, >=, <=, in, !in, contains, !contains
+											'value'    => true, // can be a single value or an array of string/number/int
+										),
+									),
+								),
+							),
+							array(
+								'id'         => 'discount_amount',
+								'type'       => 'text',
+								'default'    => 10,
+								'conditions' => array(
+									'terms' => array(
+										array(
+											'field'    => 'discount_toggle', // field ID
+											'operator' => '===', // Available operators: ===, !==, >, <, >=, <=, in, !in, contains, !contains
+											'value'    => true, // can be a single value or an array of string/number/int
+										),
+									),
+								),
+							),
+							array(
+								'id'          => 'shipping_date',
+								'type'        => 'date_time',
+								'title'       => esc_html__( 'Shipping date', 'merchant' ),
+								'placeholder' => esc_html__( 'mm/dd/yy, --:-- --', 'merchant' ),
+								'desc'        => sprintf(
+								/* Translators: %1$s: Time zone, %2$s WordPress setting link */
+									esc_html__( 'The times set above are in the %1$s timezone, according to your settings from %2$s.',
+										'merchant' ),
+									'<strong>' . wp_timezone_string() . '</strong>',
+									'<a href="' . esc_url( admin_url( 'options-general.php' ) ) . '" target="_blank">' . esc_html__( 'WordPress Settings', 'merchant' ) . '</a>'
+								),
+							),
+							array(
+								'id'      => 'partial_payment_toggle',
+								'type'    => 'switcher',
+								'title'   => __( 'Do you want to take a partial payment?', 'merchant' ),
+								'default' => 0,
+							),
+							array(
+								'id'         => 'partial_payment_type',
+								'type'       => 'radio',
+								'title'      => esc_html__( 'Payment amount', 'merchant' ),
+								'options'    => array(
+									'percentage' => esc_html__( 'Percentage', 'merchant' ),
+									'fixed'      => esc_html__( 'Fixed', 'merchant' ),
+								),
+								'default'    => 'percentage',
+								'conditions' => array(
+									'terms' => array(
+										array(
+											'field'    => 'partial_payment_toggle', // field ID
+											'operator' => '===', // Available operators: ===, !==, >, <, >=, <=, in, !in, contains, !contains
+											'value'    => true, // can be a single value or an array of string/number/int
+										),
+									),
+								),
+							),
+							array(
+								'id'         => 'partial_payment_amount',
+								'type'       => 'text',
+								'default'    => 50,
+								'conditions' => array(
+									'terms' => array(
+										array(
+											'field'    => 'partial_payment_toggle', // field ID
+											'operator' => '===', // Available operators: ===, !==, >, <, >=, <=, in, !in, contains, !contains
+											'value'    => true, // can be a single value or an array of string/number/int
+										),
+									),
+								),
+							),
+							array(
+								'id'          => 'pre_order_start',
+								'type'        => 'date_time',
+								'title'       => esc_html__( 'Pre-order starts at', 'merchant' ),
+								'placeholder' => esc_html__( 'mm/dd/yy, --:-- --', 'merchant' ),
+								'desc'        => esc_html__( 'If you want your pre-order settings to take effect immediately, leave the pre-order start empty.', 'merchant' ),
+							),
+							array(
+								'id'          => 'pre_order_end',
+								'type'        => 'date_time',
+								'title'       => esc_html__( 'Pre-order ends at', 'merchant' ),
+								'placeholder' => esc_html__( 'mm/dd/yy, --:-- --', 'merchant' ),
+								'desc'        => sprintf(
+								/* Translators: %1$s: Time zone, %2$s WordPress setting link */
+									esc_html__( 'Leave it empty if you don’t want to have an end date. The times set above are in the %1$s timezone, according to your settings from %2$s.',
+										'merchant' ),
+									'<strong>' . wp_timezone_string() . '</strong>',
+									'<a href="' . esc_url( admin_url( 'options-general.php' ) ) . '" target="_blank">' . esc_html__( 'WordPress Settings', 'merchant' ) . '</a>'
+								),
+							),
+
+							array(
+								'id'      => 'user_condition',
+								'type'    => 'select',
+								'title'   => esc_html__( 'User Condition', 'merchant' ),
+								'options' => array(
+									'all'       => esc_html__( 'All Users', 'merchant' ),
+									'customers' => esc_html__( 'Selected Users', 'merchant' ),
+									'roles'     => esc_html__( 'Selected Roles', 'merchant' ),
+								),
+								'default' => 'all',
+							),
+
+							array(
+								'id'        => 'user_condition_roles',
+								'type'      => 'select_ajax',
+								'title'     => esc_html__( 'User Roles', 'merchant' ),
+								'desc'      => esc_html__( 'This will limit the rule to users with these roles.', 'merchant' ),
+								'source'    => 'options',
+								'multiple'  => true,
+								'classes'   => array( 'flex-grow' ),
+								'options'   => Merchant_Admin_Options::get_user_roles_select2_choices(),
+								'condition' => array( 'user_condition', '==', 'roles' ),
+							),
+
+							array(
+								'id'        => 'user_condition_users',
+								'type'      => 'select_ajax',
+								'title'     => esc_html__( 'Users', 'merchant' ),
+								'desc'      => esc_html__( 'This will limit the rule to the selected customers.', 'merchant' ),
+								'source'    => 'user',
+								'multiple'  => true,
+								'classes'   => array( 'flex-grow' ),
+								'condition' => array( 'user_condition', '==', 'customers' ),
+							),
+							array(
+								'id'      => 'button_text',
+								'type'    => 'text',
+								'title'   => esc_html__( 'Button text', 'merchant' ),
+								'default' => esc_html__( 'Pre Order Now!', 'merchant' ),
+							),
+
+							array(
+								'id'      => 'additional_text',
+								'type'    => 'text',
+								'title'   => esc_html__( 'Additional information', 'merchant' ),
+								'default' => esc_html__( 'Ships on {date}.', 'merchant' ),
+							),
+
+							array(
+								'id'      => 'placement',
+								'type'    => 'radio',
+								'title'   => esc_html__( 'Placement', 'merchant' ),
+								'options' => array(
+									'before' => esc_html__( 'Before Button', 'merchant' ),
+									'after'  => esc_html__( 'After Button', 'merchant' ),
+								),
+								'default' => 'before',
+							),
+							array(
+								'id'      => 'cart_label_text',
+								'type'    => 'text',
+								'title'   => esc_html__( 'Label text on cart', 'merchant' ),
+								'default' => esc_html__( 'Ships on', 'merchant' ),
+							),
+							array(
+								'id'      => 'text-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button text color', 'merchant' ),
+								'default' => '#FFF',
+							),
+
+							array(
+								'id'      => 'text-hover-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button text color hover', 'merchant' ),
+								'default' => '#FFF',
+							),
+
+							array(
+								'id'      => 'border-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button border color', 'merchant' ),
+								'default' => '#212121',
+							),
+
+							array(
+								'id'      => 'border-hover-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button border color hover', 'merchant' ),
+								'default' => '#414141',
+							),
+
+							array(
+								'id'      => 'background-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button background color', 'merchant' ),
+								'default' => '#212121',
+							),
+
+							array(
+								'id'      => 'background-hover-color',
+								'type'    => 'color',
+								'title'   => esc_html__( 'Button background color hover', 'merchant' ),
+								'default' => '#414141',
+							),
+						),
+					),
+				),
+				'default'      => array(
+					array(
+						'layout' => 'rule-details',
+					),
+				),
+			),
 		),
-
-		array(
-			'id'      => 'additional_text',
-			'type'    => 'text',
-			'title'   => esc_html__( 'Additional information', 'merchant' ),
-			'default' => esc_html__( 'Ships on {date}.', 'merchant' ),
-		),
-
-		array(
-			'id'      => 'cart_label_text',
-			'type'    => 'text',
-			'title'   => esc_html__( 'Label text on cart', 'merchant' ),
-			'default' => esc_html__( 'Ships on', 'merchant' ),
-		),
-
-		array(
-			'id'    => 'text-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button text color', 'merchant' ),
-			'default' => '#FFF',
-		),
-
-		array(
-			'id'    => 'text-hover-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button text color hover', 'merchant' ),
-			'default' => '#FFF',
-		),
-
-		array(
-			'id'    => 'border-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button border color', 'merchant' ),
-			'default' => '#212121',
-		),
-
-		array(
-			'id'    => 'border-hover-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button border color hover', 'merchant' ),
-			'default' => '#414141',
-		),
-
-		array(
-			'id'    => 'background-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button background color', 'merchant' ),
-			'default' => '#212121',
-		),
-
-		array(
-			'id'    => 'background-hover-color',
-			'type'  => 'color',
-			'title' => esc_html__( 'Button background color hover', 'merchant' ),
-			'default' => '#414141',
-		),
-
-	),
-) );
+	)
+);
