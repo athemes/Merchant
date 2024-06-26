d91c0cdcc63b0e747aeec493c885a54461efca95
devwael <dev.ahmedwael@gmail.com>
2024-06-26 23:35:08 +0300

    Update frontend template

--- templates/modules/frequently-bought-together/single-product.php
+++ templates/modules/frequently-bought-together/single-product.php
@@ -27,7 +27,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 				?>
 				<?php if ( empty( $bundle['products'] ) ) continue ?>
                 <h3 class="merchant-frequently-bought-together-title">
-					<?php echo isset( $bundle['title'] ) ? esc_html( Merchant_Translator::translate( $bundle['title'] ) ) : esc_html__( 'Frequently Bought Together', 'merchant' ) ?>
+					<?php echo isset( $bundle['product_single_page']['title'] ) ? esc_html( Merchant_Translator::translate( $bundle['product_single_page']['title'] ) ) : esc_html__( 'Frequently Bought Together', 'merchant' ) ?>
                 </h3>
 				<div class="merchant-frequently-bought-together-bundle<?php echo ( $has_no_discount ) ? ' has-no-discount' : ''; ?>">
 					<form class="merchant-frequently-bought-together-form" data-product="<?php echo esc_attr( isset( $bundle['product_to_display'] ) ? $bundle['product_to_display'] : $parent_id ) ?>" data-bundle="<?php echo esc_attr( $key ); ?>" data-bundle-discount-type="<?php echo esc_attr( $discount_type ); ?>" data-bundle-discount-value="<?php echo esc_attr( $discount_value ); ?>">
@@ -41,8 +41,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 								?>
 								<div class="merchant-frequently-bought-together-bundle-product<?php echo $is_variable_product ? ' is-variable' : ''; ?>" data-product="<?php echo esc_attr( $product['id'] ) ?>" data-key="<?php echo esc_attr( $product_key ) ?>" data-product-price="<?php echo esc_attr( $product['price'] ); ?>" style="<?php
-								echo isset( $bundle['bundle_border_radius'] ) ? esc_attr( 'border-radius: ' . $bundle['bundle_border_radius'] . 'px;' ) : '';
-								echo isset( $bundle['bundle_border_color'] ) ? esc_attr( 'border-color: ' . $bundle['bundle_border_color'] . ';' ) : ''; ?>">
+								echo isset( $bundle['product_single_page']['bundle_border_radius'] ) ? esc_attr( 'border-radius: ' . $bundle['product_single_page']['bundle_border_radius'] . 'px;' ) : '';
+								echo isset( $bundle['product_single_page']['bundle_border_color'] ) ? esc_attr( 'border-color: ' . $bundle['product_single_page']['bundle_border_color'] . ';' ) : ''; ?>">
 									<?php echo wp_kses_post( $product['image'] ); ?>
 									<div class="merchant-frequently-bought-together-bundle-product-contents">
 										<p class="woocommerce-loop-product__title">
@@ -73,26 +73,26 @@ if ( ! defined( 'ABSPATH' ) ) {
 									</div>
 									<?php if ( $product_key !== ( count( $bundle['products'] ) - 1 ) ) : ?>
 										<div class="merchant-frequently-bought-together-bundle-product-plus" style="<?php
-										echo isset( $bundle['plus_bg_color'] ) ? esc_attr( 'background-color: ' . $bundle['plus_bg_color'] . ';' ) : '';
-										echo isset( $bundle['plus_text_color'] ) ? esc_attr( 'color: ' . $bundle['plus_text_color'] . ';' ) : ''; ?>">+</div>
+										echo isset( $bundle['product_single_page']['plus_bg_color'] ) ? esc_attr( 'background-color: ' . $bundle['product_single_page']['plus_bg_color'] . ';' ) : '';
+										echo isset( $bundle['product_single_page']['plus_text_color'] ) ? esc_attr( 'color: ' . $bundle['product_single_page']['plus_text_color'] . ';' ) : ''; ?>">+</div>
 									<?php endif; ?>
 								</div>
 							<?php endforeach; ?>
 						</div>
 						<div class="merchant-frequently-bought-together-bundle-offer">
 							<p class="merchant-frequently-bought-together-bundle-total">
-								<?php echo isset( $bundle['price_label'] ) ? esc_html( Merchant_Translator::translate( $bundle['price_label'] ) ) : esc_html__( 'Bundle price', 'merchant' ); ?>
+								<?php echo isset( $bundle['product_single_page']['price_label'] ) ? esc_html( Merchant_Translator::translate( $bundle['product_single_page']['price_label'] ) ) : esc_html__( 'Bundle price', 'merchant' ); ?>
 							</p>
 							<?php if ( $bundle_has_variable_product ) : ?>
 								<?php
 								if ( $has_no_discount ) : ?>
                                     <p class="merchant-frequently-bought-together-bundle-variable-default-message"><?php
-										echo isset( $bundle['no_variation_selected_text_has_no_discount'] )
-											? esc_html( Merchant_Translator::translate( $bundle['no_variation_selected_text_has_no_discount'] ) ) : esc_html__( 'Please select an option to see the total price.', 'merchant' ); ?></p>
+										echo isset( $bundle['product_single_page']['no_variation_selected_text_has_no_discount'] )
+											? esc_html( Merchant_Translator::translate( $bundle['product_single_page']['no_variation_selected_text_has_no_discount'] ) ) : esc_html__( 'Please select an option to see the total price.', 'merchant' ); ?></p>
 								<?php
 								else : ?>
                                     <p class="merchant-frequently-bought-together-bundle-variable-default-message"><?php
-										echo isset( $bundle['no_variation_selected_text'] ) ? esc_html( Merchant_Translator::translate( $bundle['no_variation_selected_text'] ) )
+										echo isset( $bundle['product_single_page']['no_variation_selected_text'] ) ? esc_html( Merchant_Translator::translate( $bundle['product_single_page']['no_variation_selected_text'] ) )
 											: esc_html__( 'Please select an option to see your savings.', 'merchant' ); ?></p>
 								<?php
 								endif; ?>
@@ -108,8 +108,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 							</p>
 							<?php if ( ! $has_no_discount ) : ?>
 								<p class="merchant-frequently-bought-together-bundle-save<?php echo $bundle_has_variable_product ? ' merchant-hidden' : ''; ?>">
-									<?php echo isset( $bundle['save_label'] )
-										? wp_kses( str_replace( '{amount}', wc_price( $bundle['total_discount'] ), Merchant_Translator::translate( $bundle['save_label'] ) ), merchant_kses_allowed_tags( array( 'bdi' ) ) )
+									<?php echo isset( $bundle['product_single_page']['save_label'] )
+										? wp_kses( str_replace( '{amount}', wc_price( $bundle['product_single_page']['total_discount'] ), Merchant_Translator::translate( $bundle['save_label'] ) ), merchant_kses_allowed_tags( array( 'bdi' ) ) )
 										: wp_kses(
 											/* Translators: 1. Total discount */
 											sprintf( __( 'You save: %s', 'merchant' ), wc_price( $bundle['total_discount'] ) ),
@@ -118,7 +118,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 								</p>
 							<?php endif; ?>
 							<button type="submit" name="merchant-buy-bundle" value="97" class="button alt wp-element-button merchant-add-bundle-to-cart">
-								<?php echo isset( $bundle['button_text'] ) ? esc_html( Merchant_Translator::translate( $bundle['button_text'] ) ) : esc_html__( 'Add to cart', 'merchant' ); ?>
+								<?php echo isset( $bundle['product_single_page']['button_text'] ) ? esc_html( Merchant_Translator::translate( $bundle['product_single_page']['button_text'] ) ) : esc_html__( 'Add to cart', 'merchant' ); ?>
 							</button>
 							<div class="merchant-frequently-bought-together-bundle-error"></div>
 						</div>
