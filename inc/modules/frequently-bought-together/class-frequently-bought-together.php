f7807143c0fea609feffad601953ec633a2ca9b4
devwael <dev.ahmedwael@gmail.com>
2024-08-05 21:58:06 +0300

    Add fbt tooltip

--- inc/modules/frequently-bought-together/class-frequently-bought-together.php
+++ inc/modules/frequently-bought-together/class-frequently-bought-together.php
@@ -267,6 +267,12 @@ class Merchant_Frequently_Bought_Together extends Merchant_Add_Module {
                 </div>
                 <div class="upsell-offer">
                     <div class="offer-title"><?php esc_html_e('Last chance to get {offer_quantity} x','merchant'); ?></div>
+                    <div class="merchant-tooltip">
+                        <div class="tooltip-icon">
+                            <svg fill="#a1a1a1" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"><path d="M256,64C150,64,64,150,64,256s86,192,192,192,192-86,192-192S362,64,256,64Zm-6,304a20,20,0,1,1,20-20A20,20,0,0,1,250,368Zm33.44-102C267.23,276.88,265,286.85,265,296a14,14,0,0,1-28,0c0-21.91,10.08-39.33,30.82-53.26C287.1,229.8,298,221.6,298,203.57c0-12.26-7-21.57-21.49-28.46-3.41-1.62-11-3.2-20.34-3.09-11.72.15-20.82,2.95-27.83,8.59C215.12,191.25,214,202.83,214,203a14,14,0,1,1-28-1.35c.11-2.43,1.8-24.32,24.77-42.8,11.91-9.58,27.06-14.56,45-14.78,12.7-.15,24.63,2,32.72,5.82C312.7,161.34,326,180.43,326,203.57,326,237.4,303.39,252.59,283.44,266Z"/></svg>
+                        </div>
+                        <div class="tooltip-text"><?php esc_html_e('Note: When you click ‘Add to Cart’, the item will be added to your cart and you’ll be taken to the cart page where you’ll see that a bundle discount has been applied to it. This is shown under ‘Your Savings’, and reflects a 10% discount based on the original prices of the {products}. You can then proceed to checkout as usual.','merchant'); ?></div>
+                    </div>
                     <div class="product-details">
                         <div class="product-image"></div>
                         <div class="product-info">
@@ -275,7 +281,7 @@ class Merchant_Frequently_Bought_Together extends Merchant_Add_Module {
                             <button class="add-to-order"><?php esc_html_e('Add To My Order','merchant'); ?></button>
                         </div>
                     </div>
-                    <div class="bonus-tip"><?php esc_html_e('Note: When you click ‘Add to Cart’, the item will be added to your cart and you’ll be taken to the cart page where you’ll see that a bundle discount has been applied to it. This is shown under ‘Your Savings’, and reflects a 10% discount based on the original prices of the {products}. You can then proceed to checkout as usual.','merchant'); ?></div></div>
+                    <div class="bonus-tip"></div>
                 </div>
             </div>
         </div>
