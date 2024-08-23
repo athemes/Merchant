027a5231beb5dbabd51234e1f394e34b2d104a30
Nahid <nahiddhaka07@gmail.com>
2024-08-23 21:58:11 +0600

    Allow decimals to Storewide Sale

--- inc/modules/storewide-sale/admin/options.php
+++ inc/modules/storewide-sale/admin/options.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 Merchant_Admin_Options::create( array(
-	'title'  => esc_html__( 'Discounts', 'merchant' ),
+	'title'  => esc_html__( 'Simple Discount', 'merchant' ),
 	'module' => Merchant_Storewide_Sale::MODULE_ID,
 	'fields' => array(
 		array(
@@ -91,14 +91,14 @@ Merchant_Admin_Options::create( array(
 								'percentage_discount' => esc_html__( 'Percentage', 'merchant' ),
 								'fixed_discount'      => esc_html__( 'Fixed', 'merchant' ),
 							),
-							'default' => 'percentage_discount',
+							'default' => 'percentage',
 						),
 						array(
 							'id'      => 'discount_value',
 							'type'    => 'number',
 							'default' => 10,
 							'min'     => 0,
-							'step'    => 0.1,
+							'step'    => 0.01,
 						),
 
 						array(
