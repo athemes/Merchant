31eaa5c51cee06709211b73b6a249b4ba0221d2a
devwael <dev.ahmedwael@gmail.com>
2024-09-24 17:31:34 +0300

    Prettify side-cart.js

--- assets/js/src/modules/side-cart/side-cart.js
+++ assets/js/src/modules/side-cart/side-cart.js
@@ -2,130 +2,133 @@
  * Merchant side cart
  *
  */
+jQuery(document).ready(function ($) {
+	// Toggle side cart
+	if (merchant.setting.hasOwnProperty('show_after_add_to_cart_single_product')) {
+		const isSingleProductPage = $('body.single-product').length;
+		const isNoticeVisible = $('.woocommerce-notices-wrapper').is(':visible') && !$('.woocommerce-notices-wrapper').is(':empty')
+		const isBlockNoticeVisible = $('.wc-block-components-notice-banner').is(':visible') && !$('.wc-block-components-notice-banner').is(':empty')
 
-jQuery(document).ready(function($) {
+		if (isSingleProductPage && (isNoticeVisible || isBlockNoticeVisible)) {
+			$('body').toggleClass('merchant-floating-side-mini-cart-show');
+			$(window).trigger('merchant.floating-mini-cart-resize');
+		}
+	}
 
-  if ( merchant.setting.hasOwnProperty('show_after_add_to_cart_single_product' ) ) {
-    const isSingleProductPage   = $( 'body.single-product' ).length;
-    const isNoticeVisible      = $( '.woocommerce-notices-wrapper' ).is( ':visible' ) && ! $( '.woocommerce-notices-wrapper' ).is( ':empty' )
-    const isBlockNoticeVisible = $( '.wc-block-components-notice-banner' ).is( ':visible' ) && ! $( '.wc-block-components-notice-banner' ).is( ':empty' )
+	// Add to cart AJAX event.
+	if (merchant.setting.hasOwnProperty('add_to_cart_slide_out')) {
+		$(document.body).on('added_to_cart', function (event, fragments, cart_hash, $button, $context) {
+			if ($context !== 'side-cart') {
+				$('body').toggleClass('merchant-floating-side-mini-cart-show');
+			}
+			$(window).trigger('merchant.floating-mini-cart-resize');
+		});
+	}
 
-    if ( isSingleProductPage && ( isNoticeVisible || isBlockNoticeVisible ) ) {
-      $( 'body' ).toggleClass( 'merchant-floating-side-mini-cart-show' );
-      $( window ).trigger( 'merchant.floating-mini-cart-resize' );
-    }
-  }
+	// On cart URL click
+	if (merchant.setting.hasOwnProperty('cart_url')) {
+		$('[href="' + merchant.setting.cart_url + '"]').on('click', function (e) {
+			e.preventDefault();
+			$(window).trigger('merchant.floating-mini-cart-resize');
+			$('body').toggleClass('merchant-floating-side-mini-cart-show');
+		});
+	}
 
-  // Add to cart AJAX event.
-  if (merchant.setting.hasOwnProperty('add_to_cart_slide_out')) {
-    $(document.body).on('added_to_cart', function(event, fragments, cart_hash, $button, $context) {
-      if ( $context !== 'side-cart' ) {
-        $('body').toggleClass('merchant-floating-side-mini-cart-show');
-      }
-      $(window).trigger('merchant.floating-mini-cart-resize');
-    });
-  }
+	// Update Product quantity in Side Cart
+	if (merchant.setting.hasOwnProperty('add_to_cart_slide_out') || merchant.setting.hasOwnProperty('floating_mini_cart_count')) {
+		// Update quantity on plus/minus click
+		$(document).on('click', '.js-merchant-quantity-btn', function (e) {
+			e.preventDefault();
 
-  // On cart URL click
-  if (merchant.setting.hasOwnProperty('cart_url')) {
-    $('[href="' + merchant.setting.cart_url + '"]').on('click', function(e) {
-      e.preventDefault();
-      $(window).trigger('merchant.floating-mini-cart-resize');
-      $('body').toggleClass('merchant-floating-side-mini-cart-show');
-    });
-  }
+			const $btn = $(this);
 
-  // Update Product quantity in Side Cart
-  if ( merchant.setting.hasOwnProperty( 'add_to_cart_slide_out' ) || merchant.setting.hasOwnProperty( 'floating_mini_cart_count' ) ) {
-    // Update quantity on plus/minus click
-    $( document ).on( 'click', '.js-merchant-quantity-btn', function ( e ) {
-      e.preventDefault();
+			const $input = $btn.closest('.merchant-quantity-wrap').find('.js-update-quantity');
+			if (!$input.length) {
+				return;
+			}
 
-      const $btn = $( this );
+			let quantity = +($input.val() || 1);
+			const minimum = +($input.attr('min'));
+			const maximum = +($input.attr('max'));
+			const stepSize = Math.round(parseFloat($input.attr('step')));
 
-      const $input = $btn.closest( '.merchant-quantity-wrap' ).find( '.js-update-quantity' );
-      if ( ! $input.length ) {
-        return;
-      }
+			if ($btn.hasClass('merchant-quantity-plus')) {
+				quantity += stepSize;
+				quantity = maximum && maximum !== -1 ? Math.min(quantity, maximum) : quantity;
+			} else if ($btn.hasClass('merchant-quantity-minus')) {
+				quantity -= stepSize;
+				quantity = minimum ? Math.max(quantity, minimum) : quantity;
+			}
 
-      let quantity = +( $input.val() || 1 );
-      const minimum = +( $input.attr( 'min' ) );
-      const maximum = +( $input.attr('max') );
-      const stepSize = Math.round( parseFloat( $input.attr( 'step' ) ) );
+			$input.val(quantity);
+			merchant_update_side_cart_quantity($input);
+		});
 
-      if ( $btn.hasClass( 'merchant-quantity-plus' ) ) {
-        quantity += stepSize;
-        quantity = maximum && maximum !== -1 ? Math.min( quantity, maximum ) : quantity;
-      } else if ( $btn.hasClass( 'merchant-quantity-minus' ) ) {
-        quantity -= stepSize;
-        quantity = minimum ? Math.max( quantity, minimum ) : quantity;
-      }
+		// Update quantity on input value change
+		$(document).on('input change', '.js-update-quantity', function (e) {
+			e.preventDefault();
+			merchant_update_side_cart_quantity($(this));
+		});
 
-      $input.val( quantity );
-      merchant_update_side_cart_quantity( $input );
-    } );
+		// Update quantity helper
+		let debounceTimer;
 
-    // Update quantity on input value change
-    $( document ).on( 'input change', '.js-update-quantity', function ( e ) {
-      e.preventDefault();
-      merchant_update_side_cart_quantity( $( this ) );
-    } );
+		function merchant_update_side_cart_quantity($input) {
+			const {ajax_url, side_cart_nonce} = merchant.setting || {};
 
-    // Update quantity helper
-    let debounceTimer;
-    function merchant_update_side_cart_quantity( $input ) {
-      const { ajax_url, side_cart_nonce } = merchant.setting || {};
+			if (!$input.length || !ajax_url || !side_cart_nonce) {
+				return;
+			}
 
-      if ( ! $input.length || ! ajax_url || ! side_cart_nonce ) {
-        return;
-      }
+			const cartItemKey = $input.attr('name');
+			const quantity = Math.round(parseFloat($input.val() || 1));
 
-      const cartItemKey = $input.attr( 'name' );
-      const quantity = Math.round( parseFloat( $input.val() || 1 ) );
+			const $cart_item = $input.closest('.js-side-cart-item');
 
-      const $cart_item = $input.closest( '.js-side-cart-item' );
+			// Clear previous timer
+			clearTimeout(debounceTimer);
 
-      // Clear previous timer
-      clearTimeout( debounceTimer );
+			// Set a new timer to delay the AJAX request
+			debounceTimer = setTimeout(
+				() => {
+					$.ajax({
+						type: 'POST',
+						url: ajax_url,
+						data: {
+							action: 'update_side_cart_quantity',
+							cart_item_key: cartItemKey,
+							quantity: quantity,
+							nonce: side_cart_nonce,
+						},
+						beforeSend: function () {
+							if ($cart_item.length) {
+								$cart_item.block({
+									message: null,
+									overlayCSS: {
+										background: '#fff',
+										opacity: 0.6,
+									},
+								});
+							}
+						},
+						success: (response) => {
+							if (!response || !response.fragments) {
+								return;
+							}
 
-      // Set a new timer to delay the AJAX request
-      debounceTimer = setTimeout( () => {
-        $.ajax( {
-          type: 'POST',
-          url: ajax_url,
-          data: {
-            action: 'update_side_cart_quantity',
-            cart_item_key: cartItemKey,
-            quantity: quantity,
-            nonce: side_cart_nonce,
-          },
-          beforeSend: function () {
-            if ( $cart_item.length ) {
-              $cart_item.block( {
-                message: null,
-                overlayCSS: {
-                  background: '#fff',
-                  opacity: 0.6,
-                },
-              } );
-            }
-          },
-          success: ( response ) => {
-            if ( ! response || ! response.fragments ) {
-              return;
-            }
+							$(document.body).trigger('added_to_cart', [response.fragments, response.cart_hash, $input, 'side-cart']);
 
-            $( document.body ).trigger( 'added_to_cart', [ response.fragments, response.cart_hash, $input, 'side-cart' ] );
-
-            if ( $cart_item.length ) {
-              $cart_item.unblock();
-            }
-          },
-          error: ( error ) => {
-            console.log( 'Error:', error );
-          },
-        } );
-      }, 350 );
-    }
-  }
+							if ($cart_item.length) {
+								$cart_item.unblock();
+							}
+						},
+						error: (error) => {
+							console.log('Error:', error);
+						},
+					});
+				},
+				350
+			);
+		}
+	}
 });
