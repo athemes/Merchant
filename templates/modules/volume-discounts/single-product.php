45880d85ed878b83b1397645199a07d090573ed0
Nahid <nahiddhaka07@gmail.com>
2024-11-08 21:31:33 +0600

    Add hover settings and improve CSS settings

--- templates/modules/volume-discounts/single-product.php
+++ templates/modules/volume-discounts/single-product.php
@@ -63,26 +63,19 @@ $product_id     = $product->get_id();
 			$available_variations = $is_variable ? $product->get_available_variations() : array();
 			$attributes           = $is_variable ? $product->get_variation_attributes() : array();
 			$available_variations = $is_variable ? $product->get_available_variations() : array();
-
-			if ( isset( $discount_tier['product_single_page']['table_item_text_color'] ) ) {
-                // todo: to be improved later
-				?>
-                <style>
-                    .merchant-volume-discounts-item-<?php echo esc_attr( $i ); ?> .merchant-volume-discounts-buy-label .woocommerce-Price-amount,
-                    .merchant-volume-discounts-item-<?php echo esc_attr( $i ); ?> ul .woocommerce-Price-amount {
-                        color: <?php echo esc_attr( $discount_tier['product_single_page']['table_item_text_color'] ) ; ?> !important;
-                    }
-                </style>
-				<?php
-			}
 			?>
-            <div class="<?php echo esc_attr( $item_classes ); ?>" title="<?php echo esc_attr__( 'Add offer to cart', 'merchant' ); ?>" data-in-cart="<?php
-            echo esc_attr( $in_cart ); ?>" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-offer-quantity="<?php echo esc_attr( $discount_qty ); ?>"
+            <div
+                class="<?php echo esc_attr( $item_classes ); ?>" title="<?php echo esc_attr__( 'Add offer to cart', 'merchant' ); ?>"
+                data-in-cart="<?php echo esc_attr( $in_cart ); ?>" data-product-id="<?php echo esc_attr( $product_id ); ?>" data-offer-quantity="<?php echo esc_attr( $discount_qty ); ?>"
                 data-offer-id="<?php echo esc_attr( $offer_id ); ?>" data-variations="<?php echo esc_attr( wp_json_encode( $available_variations ) ); ?>"
                 style="<?php
-			echo isset( $discount_tier['product_single_page']['table_item_bg_color'] ) ? esc_attr( 'background-color: ' . $discount_tier['product_single_page']['table_item_bg_color'] . ';' ) : '';
-			echo isset( $discount_tier['product_single_page']['table_item_border_color'] ) ? esc_attr( 'border-color: ' . $discount_tier['product_single_page']['table_item_border_color'] . ';' ) : '';
-			echo isset( $discount_tier['product_single_page']['table_item_text_color'] ) ? esc_attr( 'color: ' . $discount_tier['product_single_page']['table_item_text_color'] . ';' ) : ''; ?>">
+                    // Inline CSS variables to use in CSS. Will work per item.
+                    echo isset( $discount_tier['product_single_page']['table_item_text_color'] ) ? '--merchant-item-text-color:' . esc_attr( $discount_tier['product_single_page']['table_item_text_color'] ). ';' : '';
+                    echo isset( $discount_tier['product_single_page']['table_item_bg_color'] ) ? '--merchant-item-bg-color:' . esc_attr( $discount_tier['product_single_page']['table_item_bg_color'] ). ';' : '';
+                    echo isset( $discount_tier['product_single_page']['table_item_border_color'] ) ? '--merchant-item-border-color:' . esc_attr( $discount_tier['product_single_page']['table_item_border_color'] ). ';' : '';
+                    echo isset( $discount_tier['product_single_page']['table_item_text_color_hover'] ) ? '--merchant-item-text-color-hover:' . esc_attr( $discount_tier['product_single_page']['table_item_text_color_hover'] ). ';' : '';
+                    echo isset( $discount_tier['product_single_page']['table_item_bg_color_hover'] ) ? '--merchant-item-bg-color-hover:' . esc_attr( $discount_tier['product_single_page']['table_item_bg_color_hover'] ). ';' : '';
+                    echo isset( $discount_tier['product_single_page']['table_item_border_color_hover'] ) ? '--merchant-item-border-color-hover:' . esc_attr( $discount_tier['product_single_page']['table_item_border_color_hover'] ). ';' : ''; ?>">
                 <div class="merchant-volume-discounts-buy-label">
 					<?php
 					/**
@@ -150,9 +143,10 @@ $product_id     = $product->get_id();
                     </li>
                 </ul>
                 <div class="merchant-volume-discounts-item-label">
-                    <span class="merchant-volume-discounts-save-label" style="<?php
-                    echo isset( $discount_tier['product_single_page']['table_label_bg_color'] ) ? esc_attr( 'background-color: ' . $discount_tier['product_single_page']['table_label_bg_color'] . ';' ) : '';
-                    echo isset( $discount_tier['product_single_page']['table_label_text_color'] ) ? esc_attr( 'color: ' . $discount_tier['product_single_page']['table_label_text_color'] . ';' ) : ''; ?>">
+                    <span class="merchant-volume-discounts-save-label"
+                        style="<?php
+                        echo isset( $discount_tier['product_single_page']['table_label_text_color'] ) ? '--merchant-label-text-color:' . esc_attr( $discount_tier['product_single_page']['table_label_text_color'] ). ';' : '';
+                        echo isset( $discount_tier['product_single_page']['table_label_bg_color'] ) ? '--merchant-label-bg-color:' . esc_attr( $discount_tier['product_single_page']['table_label_bg_color'] ). ';' : '';?>">
                         <?php
                         if ( isset( $discount_tier['product_single_page']['save_label'] ) ) {
 	                        echo wp_kses(
