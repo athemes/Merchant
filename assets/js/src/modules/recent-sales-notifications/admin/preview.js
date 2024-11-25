218755a6d906f9b00585de00b0c435cc83daab2f
devwael <dev.ahmedwael@gmail.com>
2024-11-25 17:22:39 +0200

    Update admin preview script

--- assets/js/src/modules/recent-sales-notifications/admin/preview.js
+++ assets/js/src/modules/recent-sales-notifications/admin/preview.js
@@ -75,12 +75,18 @@
 	 */
 	function merchantInitPreview(e = null) {
 		let widget = $('.merchant-recent-sales-notifications-widget'),
+			textColoring = $('.merchant-field-text_coloring input:checked').val(),
 			layout = $('.merchant-field-layout input:checked').val(),
 			themeType = $('.merchant-field-theme_type input:checked').val(),
 			theme = $('.merchant-field-theme input:checked').val(),
 			customBgImage = $('.merchant-field-background_image input'),
 			bgColor = $('.merchant-field-background_color input').val(),
 			borderColor = $('.merchant-field-border_color input').val(),
+			messageColor = $('.merchant-field-message_color input').val(),
+			productNameColor = $('.merchant-field-product_name_color input').val(),
+			timeColor = $('.merchant-field-time_color input').val(),
+			hideProductImage = $('.merchant-field-hide_product_image input').is(':checked'),
+			hideProductName = $('.merchant-field-hide_product_name input').is(':checked'),
 			boxBorderRadius = $('.merchant-field-notification_box_radius input'),
 			productImageBorderRadius = $('.merchant-field-product_image_radius input'),
 			closeBtnBgColor = $('.merchant-field-close_btn_bg_color input').val(),
@@ -138,13 +144,37 @@
 		// Change product image border radius
 		document.documentElement.style.setProperty('--merchant-rsn-box-product-image-radius', productImageBorderRadius.val() + 'px');
 
-		setTimeout(function () {
-			let colorRecommender = new MerchantRecentSalesNotificationsColorRecommender({
-				widgetSelector: '.merchant-recent-sales-notifications-widget'
-			});
+		widget.find('.merchant-notification-message').removeAttr('style');
+		widget.find('.merchant-notification-title').removeAttr('style');
+		widget.find('.merchant-notification-time').removeAttr('style');
 
-			colorRecommender.colorRecommender();
+		setTimeout(function () {
+			if (textColoring === 'auto') {
+				let colorRecommender = new MerchantRecentSalesNotificationsColorRecommender({
+					widgetSelector: '.merchant-recent-sales-notifications-widget'
+				});
+
+				colorRecommender.colorRecommender();
+			}
 		}, 100);
+
+		if (textColoring === 'manual') {
+			widget.find('.merchant-notification-message').css('color', messageColor);
+			widget.find('.merchant-notification-title').css('color', productNameColor);
+			widget.find('.merchant-notification-time').css('color', timeColor);
+		}
+
+		if (hideProductImage) {
+			widget.find('.merchant-notification-image').hide();
+		} else {
+			widget.find('.merchant-notification-image').show();
+		}
+
+		if (hideProductName){
+			widget.find('.merchant-notification-title').hide();
+		}else{
+			widget.find('.merchant-notification-title').show();
+		}
 
 		$('.merchant-recent-sales-notifications-widget svg path').attr('fill', closeBtnColor);
 	}
