6418621a8c9222cf6ba4c5d9ceedd97f73dd34b6
devwael <dev.ahmedwael@gmail.com>
2025-01-08 19:02:10 +0200

    Update analytics page structure

--- admin/pages/page-analytics.php
+++ admin/pages/page-analytics.php
@@ -13,8 +13,17 @@ $reports = new Merchant_Analytics_Data_Reports();
 	                esc_html_e( 'Overall Overview', 'merchant' ); ?></span>
             </div>
             <div class="date-range">
+                <span class="merchant-analytics-loading-spinner"></span>
                 <span>
-                    <input type="text" class="date-range-input second-period">
+                    <input type="text" class="date-range-input" placeholder="<?php
+                    esc_attr_e( 'Date range', 'merchant' ); ?>">
+                </span>
+                <span class="compare-text">
+                    Comparing to
+                </span>
+                <span>
+                    <input type="text" class="date-range-input" placeholder="<?php
+                    esc_attr_e( 'Date range', 'merchant' ); ?>">
                 </span>
             </div>
         </div>
@@ -22,41 +31,48 @@ $reports = new Merchant_Analytics_Data_Reports();
 
     <div class="overview-cards">
         <div class="overview-card">
-			<?php echo wc_price(20)?>
+			<?php
+			echo wc_price( 20 ) ?>
         </div>
         <div class="overview-card">
-			<?php echo wc_price(20)?>
+			<?php
+			echo wc_price( 20 ) ?>
         </div>
         <div class="overview-card">
-			<?php echo wc_price(20)?>
+			<?php
+			echo wc_price( 20 ) ?>
         </div>
         <div class="overview-card">
-			<?php echo wc_price(20)?>
+			<?php
+			echo wc_price( 20 ) ?>
         </div>
         <div class="overview-card">
-			<?php echo wc_price(20)?>
+			<?php
+			echo wc_price( 20 ) ?>
         </div>
     </div>
 </div>
-<div class="merchant-analytics-section">
+<div class="merchant-analytics-section revenue-chart-section">
     <div class="chart-head">
         <div class="head-wrapper">
             <div class="title">
                 <span class="title-text"><?php
-					esc_html_e( 'Daily added revenue by Merchant', 'merchant' ); ?></span>
+	                esc_html_e( 'Daily added revenue by Merchant', 'merchant' ); ?></span>
             </div>
             <div class="date-range">
+                <span class="merchant-analytics-loading-spinner"></span>
                 <span>
-                    <input type="text" class="date-range-input second-period">
+                    <input type="text" class="date-range-input" placeholder="<?php
+                    esc_attr_e( 'Select date range', 'merchant' ); ?>">
                 </span>
             </div>
         </div>
     </div>
-    <div class="revenue-chart" data-period="<?php
-	echo esc_attr( wp_json_encode( $reports->get_revenue( '2024-01-01 00:00:00', '2024-12-29 23:59:59' ) ) )
+    <div class="chart" data-period="<?php
+	echo esc_attr( wp_json_encode( $reports->get_revenue_chart_report( '2024-01-01 00:00:00', '2024-12-29 23:59:59' ) ) )
 	?>"></div>
 </div>
-<div class="merchant-analytics-section">
+<div class="merchant-analytics-section aov-chart-section">
     <div class="chart-head">
         <div class="head-wrapper">
             <div class="title">
@@ -64,17 +80,19 @@ $reports = new Merchant_Analytics_Data_Reports();
 	                esc_html_e( 'Average order value', 'merchant' ); ?></span>
             </div>
             <div class="date-range">
+                <span class="merchant-analytics-loading-spinner"></span>
                 <span>
-                    <input type="text" class="date-range-input">
+                    <input type="text" class="date-range-input" placeholder="<?php
+                    esc_attr_e( 'Select date range', 'merchant' ); ?>">
                 </span>
             </div>
         </div>
     </div>
-    <div class="avg-order-value-chart" data-period="<?php
-	echo esc_attr( wp_json_encode( $reports->get_average_order_value( '2024-01-01 00:00:00', '2024-12-29 23:59:59' ) ) )
+    <div class="chart" data-period="<?php
+	echo esc_attr( wp_json_encode( $reports->get_aov_chart_report( '2024-01-01 00:00:00', '2024-12-29 23:59:59' ) ) )
 	?>"></div>
 </div>
-<div class="merchant-analytics-section">
+<div class="merchant-analytics-section impressions-chart-section">
     <div class="chart-head">
         <div class="head-wrapper">
             <div class="title">
@@ -82,13 +100,15 @@ $reports = new Merchant_Analytics_Data_Reports();
 	                esc_html_e( 'Number of impressions', 'merchant' ); ?></span>
             </div>
             <div class="date-range">
+                <span class="merchant-analytics-loading-spinner"></span>
                 <span>
-                    <input type="text" class="date-range-input">
+                    <input type="text" class="date-range-input" placeholder="<?php
+                    esc_attr_e( 'Select date range', 'merchant' ); ?>">
                 </span>
             </div>
         </div>
     </div>
-    <div class="impressions-chart" data-period="<?php
-	echo esc_attr( wp_json_encode( $reports->get_impressions( '2024-12-01 00:00:00', '2025-11-30 23:59:59' ) ) )
+    <div class="chart" data-period="<?php
+	echo esc_attr( wp_json_encode( $reports->get_impressions_chart_report( '2024-12-01 00:00:00', '2025-11-30 23:59:59' ) ) )
 	?>"></div>
 </div>
