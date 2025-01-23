6f815cd08b18e5d34bc3c47b535cc54bfc2bc8c0
devwael <dev.ahmedwael@gmail.com>
2025-01-23 21:08:03 +0200

    Add recently viewed products analytics flag

--- inc/modules/recently-viewed-products/class-recently-viewed-products.php
+++ inc/modules/recently-viewed-products/class-recently-viewed-products.php
@@ -29,6 +29,13 @@ class Merchant_Recently_Viewed_Products extends Merchant_Add_Module {
 	public static $is_module_preview = false;
 
 	/**
+	 * Set the module as having analytics.
+	 *
+	 * @var bool
+	 */
+	protected $has_analytics = true;
+
+	/**
 	 * Constructor.
 	 * 
 	 */
