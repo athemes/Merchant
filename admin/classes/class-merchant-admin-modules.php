95a675843a6f75231badf70dc0b64a95d3220492
devwael <dev.ahmedwael@gmail.com>
2025-03-18 16:04:31 +0200

    Add module docs link

--- admin/classes/class-merchant-admin-modules.php
+++ admin/classes/class-merchant-admin-modules.php
@@ -53,7 +53,7 @@ if ( ! class_exists( 'Merchant_Admin_Modules' ) ) {
 					'icon' => Merchant_SVG_Icons::get_svg_icon( 'module-pre-orders' ),
 					'title' => esc_html__( 'Complementary Products', 'merchant' ),
 					'desc' => esc_html__( 'Offer complementary products with a main product to increase order value', 'merchant' ),
-					'tutorial_url' => '#',
+					'tutorial_url' => 'https://docs.athemes.com/article/merchant-complementary-products/',
 				),
 				'wait-list' => array(
 					'pro' => true,
